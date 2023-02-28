249dd0c7a8a8b68ba8119ab133220b478b81f45f
Jorge Ferrando <jorgeparent@gmail.com>
2023-02-28 17:21:38 +0100

    create folder for modals

---
+++ 12-modals/src/Details.jsx
@@ -0,0 +1,45 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+
+const Details = () => {
+    const { id } = useParams();
+    const results = useQuery(["details", id], fetchPet);
+
+    if (results.isLoading) {
+        return (
+            <div className="loading-pane">
+                <h2 className="loader">🧿</h2>
+            </div>
+        );
+    }
+
+    const pet = results.data.pets[0];
+
+    return (
+        <div className="details">
+            <Carousel images={pet.images} />
+            <div>
+                <h1>{pet.name}</h1>
+                <h2>
+                    {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
+                    <button>Adopt {pet.name}</button>
+                    <p>{pet.description}</p>
+                </h2>
+            </div>
+        </div>
+    );
+};
+
+function DetailsErrorBoundary(props) {
+    //it is the only case when you use the chain operator to pass props
+    return (
+        <ErrorBoundary>
+            <Details {...props} />
+        </ErrorBoundary>
+    );
+}
+
+export default DetailsErrorBoundary;

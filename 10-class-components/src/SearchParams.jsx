7ce5f3c83b38ba846d518b62852f8aa0e58b80db
Jorge Ferrando <jorgeparent@gmail.com>
2023-02-28 16:39:01 +0100

    create new folter for class components topic

---
+++ 10-class-components/src/SearchParams.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import Results from "./Results";
+import useBreedList from "./useBreedList";
+import fetchSearch from "./fetchSearch";
+const ANIMALS = ["dog", "bird", "cat", "rabbit", "reptile"];
+
+const SearchParams = () => {
+    const [requestParams, setRequestParams] = useState({
+        location: "",
+        animal: "",
+        breed: "",
+    });
+
+    const [animal, setAnimal] = useState("");
+    const [breeds] = useBreedList(animal);
+    const results = useQuery(["search", requestParams], fetchSearch);
+
+    const pets = results?.data?.pets ?? [];
+
+    return (
+        <div className="search-params">
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    const formData = new FormData(e.target);
+                    const obj = {
+                        animal: formData.get("animal") ?? "",
+                        breed: formData.get("breed") ?? "",
+                        location: formData.get("location") ?? "",
+                    };
+                    setRequestParams(obj);
+                }}
+            >
+                <label htmlFor="location">
+                    Location
+                    <input
+                        id="location"
+                        placeholder="Location"
+                        name="location"
+                    />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select
+                        id="animal"
+                        name="animal"
+                        value={animal}
+                        onChange={(e) => setAnimal(e.target.value)}
+                    >
+                        <option />
+                        {ANIMALS.map((a) => (
+                            <option key={a}>{a}</option>
+                        ))}
+                    </select>
+                </label>
+                <label htmlFor="breed">
+                    Breed
+                    <select
+                        id="breed"
+                        disabled={breeds.length === 0}
+                        name="breed"
+                    >
+                        <option />
+                        {breeds.map((a) => (
+                            <option key={a}>{a}</option>
+                        ))}
+                    </select>
+                </label>
+                <button>Submit</button>
+            </form>
+
+            <Results pets={pets} />
+        </div>
+    );
+};
+
+export default SearchParams;
